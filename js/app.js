fedfa5e86e60db13b279d1624f840bced098ceae
Josh Rufin <joshrufin@gmail.com>
2017-05-16 11:55:44 -0400

    working on css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 var app = angular.module("ticketKiosk", ["ui.bootstrap"]);
+
+
+
+app.controller('ticketCtrl', function ($scope){
+  console.log('controller is working');
+
+  $scope.confirmedList = [];
+  $scope.price = 2.50;
+  $scope.amount = 1;
+
+  $scope.getTotal = function (confirmed) {
+    console.log('this function is working');
+    $scope.confirmedList.push({name: confirmed.name, amount: confirmed.amount });
+    console.log($scope.confirmedList);
+
+
+
+  }
+});
